cade7a129ae5b9791bf17dbc56aa893663bd135c
Zach Orlovsky <sadorlovsky@gmail.com>
2017-04-24 17:06:53 +0300

    dont use cluster and pretty logger

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 const Koa = require('koa')
 const config = require('./config')
 const routes = require('./routes')
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
 const bodyParser = require('koa-bodyparser')
 const passport = require('koa-passport')
 const bunyan = require('bunyan')
-const log = bunyan.createLogger({name: 'route'})
+const log = bunyan.createLogger({name: 'platform'})
 const cors = require('koa2-cors')
 
-if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-} else {
-  let app = new Koa()
-  app.use(bodyParser())
-  app.use(passport.initialize())
-  app.use(passport.session())
-  app.use(cors())
+const app = new Koa()
+app.use(bodyParser())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cors())
 
-  routes(app)
+routes(app)
 
-  app.listen(config.server.port, () => {
-    log.info(`app is running on port: ${config.server.port}`)
-  })
-}
+app.listen(config.server.port, () => {
+  log.info(`app is running on port: ${config.server.port}`)
+})
