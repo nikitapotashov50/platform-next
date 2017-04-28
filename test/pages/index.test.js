9260def5055d42b19d995035bf0935c5eaa7ae01
Zach Orlovsky <sadorlovsky@gmail.com>
2017-04-28 06:21:57 +0300

    enzyme test

--- test/pages/index.test.js
+++ test/pages/index.test.js
@@ -1,8 +1,26 @@
 import test from 'ava'
 import render from 'react-test-renderer'
+import React from 'react'
+import { mount } from 'enzyme'
 import IndexPage from '../../pages/index'
 
-test('Index page', t => {
+test('Index page snapshot', t => {
   const tree = render.create(<IndexPage />).toJSON()
   t.snapshot(tree)
+})
+
+test('Index page state', t => {
+  const wrapper = mount(<IndexPage />)
+  t.deepEqual(wrapper.state(), {})
+
+  const changeEvent = {
+    target: {
+      value: 'test'
+    }
+  }
+
+  wrapper.find('input').simulate('change', changeEvent)
+  t.deepEqual(wrapper.state(), {
+    login: 'test'
+  })
 })
