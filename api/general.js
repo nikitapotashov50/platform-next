ae299b195b42290da5229f53b67ee7642b220ff9
Zach Orlovsky <sadorlovsky@gmail.com>
2017-04-28 05:44:48 +0300

    add nyc

--- api/general.js
+++ api/general.js
@@ -1,5 +1,4 @@
 const Router = require('koa-router')
-const passport = require('koa-passport')
 const { messages } = require('../factories')
 const services = require('../services')
 
@@ -9,15 +8,10 @@ router.get('/', (ctx, next) => {
   ctx.body = messages.generic.success
 })
 
-router.post('/login', passport.authenticate('local'), async ctx => {
-  console.log('is auth', ctx.isAuthenticated())
-  // console.log('is not auth', ctx.isUnauthenticated())
-  // console.log('user', ctx.state.user)
-  // const login = JSON.parse(ctx.request.body)
-  // if (login && login === 'хуй') {
-  //   await ctx.login()
-  // }
-  // ctx.status = 200
+router.post('/login', async ctx => {
+  ctx.body = {
+    ok: true
+  }
 })
 
 router.get('/version', services.general.getAPIVersionService)
--- test/api/general.test.js
+++ test/api/general.test.js
@@ -7,11 +7,27 @@ const app = new Koa()
 app.use(generalRouter.routes())
 app.use(generalRouter.allowedMethods())
 
-test('General API', async t => {
+test('General API: GET /', async t => {
   const res = await request(app.listen()).get('/')
   t.is(res.status, 200)
   t.deepEqual(res.body, {
     status: 200,
     success: true
+  })
+})
+
+test('General API: GET /version', async t => {
+  const res = await request(app.listen()).get('/version')
+  t.is(res.status, 200)
+  t.deepEqual(res.body, {
+    version: 'v1'
+  })
+})
+
+test('General API: POST /login', async t => {
+  const res = await request(app.listen()).post('/login')
+  t.is(res.status, 200)
+  t.deepEqual(res.body, {
+    ok: true
   })
 })
