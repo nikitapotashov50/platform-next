23c021af0ee56d3d6e8c98e9162cc6d7253c6efe
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-04 12:57:45 +0300

    update config

--- config/index.js
+++ config/index.js
@@ -1,24 +1,19 @@
 require('dotenv').config()
 
-/**
- * root config
- * @type {factory}
- */
-
 module.exports = {
   server: {
     host: process.env.HOST || 'localhost',
-    port: process.env.PORT || 3001
+    port: process.env.PORT || 3000
   },
   db: {
-    uri: process.env.DB_URI || 'mysql://root:root@localhost:32774/test'
+    uri: process.env.DB_URI
   },
   api: {
     version: process.env.API_VERSION || 'v1',
     session_key: process.env.SESSION_KEY
   },
   bmapi: {
-    client_id: 'renat.biktagirov',
-    client_secret: '6NbQvMElYMcBbOVWie7a1Bs4rfVt9FpNY4V4Fl6EEGt4xTEUa1K0ugMohlemqFQ5'
+    client_id: process.env.BM_API_CLIENT_ID,
+    client_secret: process.env.BM_API_CLIENT_SECRET
   }
 }
