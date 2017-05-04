4627fa13209661f2d395842c5d119cef3f772b94
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-04 12:59:36 +0300

    clean code

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,5 @@
 const requireAll = require('require-all')
 
-/**
- * @description controller's factory
- * @type {module}
- */
-
 module.exports = requireAll({
   dirname: __dirname,
   filter: /(.+Controller)\.js$/,
