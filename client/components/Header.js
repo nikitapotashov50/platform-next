d72a6e156b09776bddb5fe6ebee891c9b2eb5a7c
Степан Юринов <pdll@Stepans-MacBook-Pro.local>
2017-05-04 16:14:21 +0300

    pcss

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,19 +1,50 @@
 import Link from 'next/link'
 import { connect } from 'react-redux'
 
-const Header = ({ user }) => {
-  if (user) {
-    return <div>Приветики, {user.name}</div>
-  } else {
-    return (
-      <div>
-        <Link href='/login'>
-          <a>Войти</a>
-        </Link>
+const Header = ({ user }) => (
+  <header className="app-header noPrint">
+    <div className="app-header__wrap">
+
+      <div className="app-header__block app-header__block_menu">
+        
+        <ul className="menu">
+          <li className="menu__item menu__item_no_padding-left">
+            <Link href="/">
+              <a className="menu__link">
+                <img src="~assets/img/logo.png" alt="Система" style={{ display: 'inline-block', width: '41px', height: '36px' }} />
+              </a>
+            </Link>
+          </li>
+
+          <li className="menu__item">
+            <Link href="/">
+              <a className="menu__link">Отчеты</a>
+            </Link>
+          </li>
+          
+          { user && (
+            <li className="menu__item">
+              <Link href="/">
+                <a className="menu__link">Задания</a>
+              </Link>
+            </li>
+          )}
+
+          <li className="menu__item">
+            <Link href="/">
+              <a className="menu__link">Рейтинг</a>
+            </Link>
+          </li>    
+        </ul>
       </div>
-    )
-  }
-}
+
+      <div className="app-header__block app-header__block_menu">
+        
+      </div>
+      
+    </div>
+  </header>
+)
 
 const mapStateToProps = state => {
   return {
