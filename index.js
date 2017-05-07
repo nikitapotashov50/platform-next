511710e4805479f02f3fe34e5b787667ef994a4b
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-07 16:53:25 +0300

    koa-session

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('koa2-cors')
 // const CSRF = require('koa-csrf')
 const helmet = require('koa-helmet')
 const bodyParser = require('koa-bodyparser')
-const session = require('koa-session2')
+const session = require('koa-session')
 const koaBunyanLogger = require('koa-bunyan-logger')
 const config = require('./config')
 const routes = require('./server/routes')
@@ -17,15 +17,17 @@ client.prepare().then(() => {
   const server = new Koa()
   const log = bunyan.createLogger({ name: 'platform' })
 
+  server.keys = [config.api.session_key]
+
   server.use(koaBunyanLogger(log))
   server.use(helmet())
   server.use(bodyParser())
   server.use(session({
-    key: config.api.session_key,
-    // secure: true,
-    httpOnly: true
-    // signed: true
-  }))
+    key: 'sess',
+    // secure: true, // Need HTTPS on development
+    httpOnly: true,
+    signed: true
+  }, server))
   server.use(cors())
 
   server.use(async (ctx, next) => {
