f3a2ba945b899cae0768e4f71cc37fbc4dea3665
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-07 04:13:10 +0300

    clean index page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import Link from 'next/link'
 import DefaultLayout from '../client/layouts/default'
 
 class IndexPage extends Component {
   render () {
     return (
-      <div>
-        Главная
-        <Link href='/posts' prefetch><a>Посты</a></Link>
-      </div>
+      <div />
     )
   }
 }
