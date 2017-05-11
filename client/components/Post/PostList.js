f14204f5477531a4c1742a9bf68b4c2513367267
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-11 17:42:04 +0300

    load more offset

--- client/components/Post/PostList.js
+++ client/components/Post/PostList.js
@@ -1,20 +1,37 @@
+import React, { Component } from 'react'
 import Waypoint from 'react-waypoint'
 import { connect } from 'react-redux'
 import { loadMore } from '../../redux/posts'
 import Post from './Post'
 
-let _offset = 20
+class PostList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      offset: 20
+    }
+    this.scrollDownHandle = this.scrollDownHandle.bind(this)
+  }
 
-const PostList = ({ posts, loadMore }) => (
-  <div>
-    {posts.map(post => <Post key={post.id} {...post} />)}
-    <Waypoint onEnter={() => {
-      loadMore(_offset)
-      _offset = _offset * 2
-    }} />
-    <div>Загрузка</div>
-  </div>
-)
+  scrollDownHandle () {
+    this.props.loadMore(this.state.offset)
+    this.setState({
+      offset: this.state.offset * 2
+    })
+  }
+
+  render () {
+    const { posts } = this.props
+
+    return (
+      <div>
+        {posts.map(post => <Post key={post.id} {...post} />)}
+        <Waypoint onEnter={this.scrollDownHandle} />
+        <div>Загрузка</div>
+      </div>
+    )
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   loadMore: offset => dispatch(loadMore(offset))
