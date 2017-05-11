6bc7204045a38b0331f667258f9c9ee4631f2cf2
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-11 13:20:16 +0300

    update

--- client/hocs/UserProfile.js
+++ client/hocs/UserProfile.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { server } from '../../config'
 import { getUserInfo, userNotFound } from '../redux/profile'
 
 export default Next => {
@@ -12,7 +13,7 @@ export default Next => {
 
     if (!state.profile.user || (state.profile.user.name !== query.username)) {
       let { data } = await new Promise(async (resolve, reject) => {
-        let res = await axios.get('http://localhost:3001/api/user/' + query.username)
+        let res = await axios.get(`http://${server.host}:${server.port}/api/user/${query.username}`)
         setTimeout(() => { resolve(res) }, 1000)
       })
 
--- client/tools/start_i18n.js
+++ client/tools/start_i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next'
 import axios from 'axios'
+import { server } from '../../config'
 
 const startI18n = file => i18n.init({
   fallbackLng: 'ru',
@@ -10,7 +11,7 @@ const startI18n = file => i18n.init({
 })
 
 export async function getTranslations (lang) {
-  const { data } = await axios.get(`http://localhost:3001/static/i18n/${lang}.json`)
+  const { data } = await axios.get(`http://${server.host}:${server.port}/static/i18n/${lang}.json`)
 
   return {
     [lang]: {
