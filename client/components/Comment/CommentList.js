a36864c90b6d16777c990ed4dcee992ab60d907e
Zach Orlovsky <sadorlovsky@gmail.com>
2017-05-12 15:10:26 +0300

    wip

--- client/components/Comment/CommentList.js
+++ client/components/Comment/CommentList.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size, takeRight } from 'lodash'
 import Comment from './Comment'
 
 class CommentList extends Component {
-  render () {
-    const { comments } = this.props
+  constructor (props) {
+    super(props)
+    this.state = {
+      all: false,
+      showAllButton: size(this.props.comments) > 3
+    }
+  }
 
-    if (isEmpty(comments)) {
+  render () {
+    let comments = []
+
+    if (isEmpty(this.props.comments)) {
       return null
+    }
+
+    if (!this.state.all && this.state.showAllButton) {
+      comments = takeRight(this.props.comments, 3)
+    } else {
+      comments = this.props.comments
     }
 
     return (
       <div>
+        {this.state.showAllButton && <button
+          className='show-more-comments'
+          onClick={() => {
+            this.setState({
+              all: !this.state.all
+            })
+          }}>
+          Показать {this.state.all ? 'меньше' : 'больше'}
+        </button>}
         {comments.map(comment => (
           <Comment key={comment.id} {...comment} />
         ))}
+
+        <style jsx>{`
+          .show-more-comments {
+            height: 50px;
+            width: 100%;
+            background: #ccc;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 18px;
+            cursor: pointer;
+            transition: background 0.2s ease;
+          }
+
+          .show-more-comments:hover {
+            background: #666;
+          }
+        `}</style>
       </div>
     )
   }
