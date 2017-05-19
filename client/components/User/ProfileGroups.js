d28e24198766a9a30a26e1a14ac2cb69e7ecad5b
qkish <gqkish@gmail.com>
2017-05-20 00:33:48 +0300

    Link to groups Disabled

--- client/components/User/ProfileGroups.js
+++ client/components/User/ProfileGroups.js
@@ -9,14 +9,14 @@ export default ({ groups }) => (
       <div className='user-groups'>
 
         { groups.map(group => (
-          <Link href={'/groups?id=' + group.id} as={'/groups/' + group.id} key={'user-group-' + group.id}>
+         // <Link href={'/groups?id=' + group.id} as={'/groups/' + group.id} key={'user-group-' + group.id}>
             <a className='user-groups__item'>
               <img className='user-groups__image' src={group.image || '/static/img/logo.png'} alt={group.title} />
               <div className='user-groups__body'>
                 <span className='user-groups__link'>{group.title}</span>
               </div>
             </a>
-          </Link>
+         // </Link>
         )) }
 
       </div>
