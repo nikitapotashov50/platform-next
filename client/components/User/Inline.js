2b737b36b1004ee801cd68ef801c77272d5070f2
ikolcov <ikolcov@yandex.ru>
2017-05-19 11:05:49 +0300

    transformed UserInline component to fit groups also

--- client/components/User/Inline.js
+++ client/components/User/Inline.js
@@ -1,51 +1,35 @@
 import Img from 'react-image'
 import Link from 'next/link'
-import moment from 'moment'
 import numeral from 'numeral'
 
 const formatMoney = money => numeral(money).format('0,0')
 
-export default ({ money, subscribeButtons = false, date, user, small, goal }) => {
+export default ({ money, picture, title, small, description, link }) => {
   let bodyClasses = [ 'user-inline__body' ]
-
-  if (subscribeButtons) bodyClasses.push('user-inline__body_width_thin')
+  let { href, path } = link
 
   return (
     <div className='user-inline'>
-      { user && (
-        <Link href={'/user?username=' + user.name} as={'/@' + user.name}>
-          <a className={[ 'user-inline__image-link', small ? 'user-inline__image-link_small' : '' ].join(' ')}>
-            <Img src={[ user.picture_small, '/static/img/user.png' ]} className={[ 'user-inline__image', small ? 'user-inline__image_small' : '' ].join(' ')} alt={`${user.first_name} ${user.last_name}`} />
-          </a>
-        </Link>
-      ) }
+
+      <Link href={href} as={path}>
+        <a className={[ 'user-inline__image-link', small ? 'user-inline__image-link_small' : '' ].join(' ')}>
+          <Img src={[ picture, '/static/img/user.png' ]} className={[ 'user-inline__image', small ? 'user-inline__image_small' : '' ].join(' ')} alt={title} />
+        </a>
+      </Link>
 
       <div className={bodyClasses.join(' ')}>
         <div className='user-inline__title-block'>
-          { user && user.name && (
-            <Link href={'/user?username=' + user.name} as={'/@' + user.name}>
-              <a className='user-inline__title'>{user.first_name + ' ' + user.last_name}</a>
+          { title && (
+            <Link href={href} as={path}>
+              <a className='user-inline__title'>{title}</a>
             </Link>
           )}
 
-          { !user && <span className='user-inline__title'>Annonymous</span>}
-
-          { date && (<span className='user-inline__when'>{ moment(date).from(moment()) }</span>)}
-        </div>
-
-        <div className='user-inline__info'>
-          { user && goal ? goal.occupation : '' }
+          { !title && <span className='user-inline__title'>Annonymous</span>}
         </div>
       </div>
 
-      { (user && subscribeButtons) && (
-        <div className='user-inline__buttons'>
-          <button className='myBtn myBtn_small'>Подписаться</button>
-          <button className='myBtn myBtn_small'>Заблокировать</button>
-        </div>
-      )}
-
-      { user && (<div className='user-inline__money'>{formatMoney(money)} ₽</div>)}
+      { money && (<div className='user-inline__money'>{formatMoney(money)} ₽</div>)}
     </div>
   )
 }
