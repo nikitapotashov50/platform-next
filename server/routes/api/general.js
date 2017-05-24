029467369e8b11d03884df911a2085019d866dc3
Степан Юринов <pdll@Stepans-MacBook-Pro.local>
2017-05-24 14:06:07 +0300

    refactor

--- server/routes/api/general.js
+++ server/routes/api/general.js
@@ -1,10 +1,58 @@
+const moment = require('moment')
 const services = require('../../services')
-const { models } = require('../../models')
+const { models, cached } = require('../../models')
 
 module.exports = router => {
   router.post('/login', async ctx => {
     ctx.body = {
       ok: true
+    }
+  })
+
+  router.get('/refactor', async ctx => {
+    let defaultProgram = await models.Program.findOne({
+      where: {
+        alias: 'defaut'
+      }
+    })
+
+    if (!defaultProgram) {
+      defaultProgram = await models.Program.create({
+        alias: 'default',
+        title: 'БМ Платформа',
+        is_enabled: true,
+        start_at: moment('2015-01-01').format('YYYY-MM-DD HH:mm:ss'),
+        finish_at: moment('2115-01-01').format('YYYY-MM-DD HH:mm:ss')
+      })
+    }
+
+    let programPosts = await models.Post.findAll({
+      attributes: [ 'id' ],
+      where: {
+        '$Programs.id$': { $eq: null }
+      },
+      include: [
+        {
+          model: models.Program
+        }
+      ],
+      order: [
+        [ 'created_at', 'desc' ]
+      ]
+    })
+
+    await Promise.all(programPosts.map(async el => {
+      await new Promise((resolve, reject) => {
+        el.addPrograms([ defaultProgram ])
+      })
+    }))
+
+    ctx.body = {
+      status: 200,
+      result: {
+        defaultProgram,
+        programPosts
+      }
     }
   })
 
@@ -28,7 +76,7 @@ module.exports = router => {
   })
 
   router.get('/user/:username', async ctx => {
-    let user = await models.User.findOne({
+    let user = await cached.User.findOne({
       where: {
         name: ctx.params.username
       },
@@ -76,10 +124,10 @@ module.exports = router => {
         status: 404
       }
     } else {
-      let groups = user.get('Groups')
-      let subscriptions = user.get('Subscriptions')
-      let subscribers = user.get('Subscribers')
-      let goal = user.get('Goals')
+      let groups = user.Groups
+      let subscriptions = user.Subscriptions
+      let subscribers = user.Subscribers
+      let goal = user.Goals
 
       ctx.body = {
         user,
