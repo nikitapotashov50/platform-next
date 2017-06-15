09327dbbbcff745a7702afb124d40fa0d1ae93e9
qkish <gqkish@gmail.com>
2017-06-15 22:04:13 +0300

    RUbles Fix

--- client/components/Tasks/replyContent/goal.js
+++ client/components/Tasks/replyContent/goal.js
@@ -5,7 +5,7 @@ const formatMoney = value => numeral(value || 0).format('0,0')
 export default ({ data }) => (
   <div>
     <div><strong>Ниша:</strong> {data.occupation}</div>
-    <div><strong>Ваша точка А:</strong> {formatMoney(data.a)} ₽</div>
-    <div><strong>Ваша точка Б на два месяца:</strong> {formatMoney(data.a)} ₽</div>
+    <div><strong>Ваша точка А, в рублях:</strong> {formatMoney(data.a)} ₽</div>
+    <div><strong>Ваша точка Б на два месяца, в рублях:</strong> {formatMoney(data.a)} ₽</div>
   </div>
 )
