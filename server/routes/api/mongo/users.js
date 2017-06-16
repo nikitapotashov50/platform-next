c1a7f678f53e57ba72116c0466f0f283b6ecdf09
Zach Orlovsky <sadorlovsky@gmail.com>
2017-06-16 15:49:46 +0300

    nps rating work in progress

--- server/routes/api/mongo/users.js
+++ server/routes/api/mongo/users.js
@@ -1,36 +1,68 @@
 const { models } = require('mongoose')
-const { extend, pick } = require('lodash')
+const { extend, pick, toLower } = require('lodash')
 const { getBalance } = require('../../../controllers/tokenController')
 
 module.exports = router => {
-  router.bridge('/list', router => {
-    router.get('/', async ctx => {
-      let { programId, notIn } = ctx.query
+  // router.bridge('/list', router => {
+  //   router.get('/', async ctx => {
+  //     let { programId, notIn } = ctx.query
+  //
+  //     let params = {}
+  //     if (programId) params['programs.programId'] = { $in: [ Number(programId) ] }
+  //     if (notIn) params._id = { $nin: notIn.split(',') }
+  //
+  //     let users = await models.Users
+  //       .find(params)
+  //       .select('_id first_name last_name picture_small name goals')
+  //       .populate({
+  //         path: 'goals',
+  //         match: { closed: false },
+  //         select: 'a b occupation',
+  //         options: {
+  //           limit: 1,
+  //           sort: { created: -1 }
+  //         }
+  //       })
+  //       .limit(20)
+  //       .lean()
+  //
+  //     ctx.body = {
+  //       status: 200,
+  //       result: { users }
+  //     }
+  //   })
+  // })
 
-      let params = {}
-      if (programId) params['programs.programId'] = { $in: [ Number(programId) ] }
-      if (notIn) params._id = { $nin: notIn.split(',') }
+  router.get('/list', async ctx => {
+    let { limit = 20, userIds, searchString } = ctx.request.query
 
+    let query = {}
+    if (userIds) query._id = { $in: userIds.split(',') }
+    if (searchString !== '') {
+      searchString = toLower(decodeURIComponent(searchString))
+      query['$or'] = [
+        { name: { $regex: searchString } },
+        { first_name: { $regex: searchString, $options: 'i' } },
+        { last_name: { $regex: searchString } },
+        { email: { $regex: searchString } }
+      ]
+    }
+
+    try {
       let users = await models.Users
-        .find(params)
-        .select('_id first_name last_name picture_small name goals')
-        .populate({
-          path: 'goals',
-          match: { closed: false },
-          select: 'a b occupation',
-          options: {
-            limit: 1,
-            sort: { created: -1 }
-          }
-        })
-        .limit(20)
+        .find(query)
+        .select('_id name first_name last_name picture_small')
         .lean()
+        .limit(Number(limit))
 
       ctx.body = {
         status: 200,
         result: { users }
       }
-    })
+    } catch (e) {
+      ctx.log.info(e)
+      ctx.body = { status: 500, message: e }
+    }
   })
 
   router.bridge('/:username', router => {
