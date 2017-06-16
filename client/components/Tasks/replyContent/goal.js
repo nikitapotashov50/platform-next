a1401a402ab0ae0c8372845d3eedea680ef4dd49
qkish <gqkish@gmail.com>
2017-06-16 23:27:38 +0300

    Point B fix

--- client/components/Tasks/replyContent/goal.js
+++ client/components/Tasks/replyContent/goal.js
@@ -6,6 +6,6 @@ export default ({ data }) => (
   <div>
     <div><strong>Ниша:</strong> {data.occupation}</div>
     <div><strong>Ваша точка А, в рублях:</strong> {formatMoney(data.a)} ₽</div>
-    <div><strong>Ваша точка Б на два месяца, в рублях:</strong> {formatMoney(data.a)} ₽</div>
+    <div><strong>Ваша точка Б на два месяца, в рублях:</strong> {formatMoney(data.b)} ₽</div>
   </div>
 )
