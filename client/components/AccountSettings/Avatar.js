6a9460b1219c26a031820a39d79cbc9cc3604458
Zach Orlovsky <sadorlovsky@gmail.com>
2017-06-16 16:08:24 +0300

    uploading status

--- client/components/AccountSettings/Avatar.js
+++ client/components/AccountSettings/Avatar.js
@@ -1,50 +1,67 @@
+import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 import { isUndefined } from 'lodash'
 import axios from 'axios'
 import md5 from 'blueimp-md5'
 
-let dropzoneRef
+class AvatarSettings extends Component {
+  constructor (props) {
+    super(props)
 
-const AvatarSettings = ({ user, onChange, affected }) => (
-  <Dropzone
-    disableClick
-    multiple
-    style={{}}
-    ref={node => { dropzoneRef = node }}
-    onDrop={async ([file]) => {
-      const formData = new window.FormData()
-      formData.append('file', file)
-      formData.append('hash', md5(file.preview))
-      formData.append('isAvatar', true)
-      const { data } = await axios.post('/api/attachment', formData)
-      onChange('picture_small', data.url)
-    }}
-  >
-    {isUndefined(affected.picture_small) ? (
-      <div>
-        {user.picture_small === '' ? (
-          <div>Нет аватара</div>
-        ) : (
-          <img src={user.picture_small} />
+    this.state = {
+      uploading: false
+    }
+  }
+  render () {
+    const { user, onChange, affected } = this.props
+
+    return (
+      <Dropzone
+        disableClick
+        multiple
+        style={{}}
+        ref={node => { this.dropzoneRef = node }}
+        onDrop={async ([file]) => {
+          this.setState({ uploading: true })
+          const formData = new window.FormData()
+          formData.append('file', file)
+          formData.append('hash', md5(file.preview))
+          formData.append('isAvatar', true)
+          const { data } = await axios.post('/api/attachment', formData)
+          this.setState({ uploading: false })
+          onChange('picture_small', data.url)
+        }}
+      >
+        {this.state.uploading && (
+          <div>Загрузка...</div>
         )}
-      </div>
-    ) : (
-      <div>
-        <img src={affected.picture_small} />
-      </div>
-    )}
+        {isUndefined(affected.picture_small) ? (
+          <div>
+            {user.picture_small === '' ? (
+              <div>Нет аватара</div>
+            ) : (
+              <img src={user.picture_small} />
+            )}
+          </div>
+        ) : (
+          <div>
+            <img src={affected.picture_small} />
+          </div>
+        )}
 
-    <button onClick={() => { dropzoneRef.open() }}>Выбрать файл</button>
+        <button onClick={() => { this.dropzoneRef.open() }}>Выбрать файл</button>
 
-    <style jsx>{`
-      img {
-        width: 300px;
-      }
-      button {
-        cursor: pointer;
-      }
-    `}</style>
-  </Dropzone>
-)
+        <style jsx>{`
+          img {
+            width: 300px;
+          }
+          button {
+            cursor: pointer;
+          }
+        `}</style>
+      </Dropzone>
+    )
+  }
+}
 
 export default AvatarSettings
