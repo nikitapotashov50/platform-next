25555e839a621cf609bad0da5273eb53dfd99791
Zach Orlovsky <sadorlovsky@gmail.com>
2017-06-16 15:49:46 +0300

    nps rating work in progress

---
+++ pages/npsrating.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import UserInline from '../client/components/User/Inline'
+import Page from '../client/hocs/Page'
+import Panel from '../client/elements/Panel'
+import FeedLayout from '../client/layouts/feed'
+
+class NPSRatings extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchQuery: '',
+      users: []
+    }
+  }
+
+  async fetchUsers (searchQuery = '') {
+    const { data } = await axios(`/api/mongo/users/list?searchString=${searchQuery}`)
+    return data.result.users
+  }
+
+  async componentDidMount () {
+    const users = await this.fetchUsers()
+
+    this.setState({
+      users: users
+    })
+  }
+
+  render () {
+    return (
+      <FeedLayout>
+        <Panel>
+          <div className='rating-list'>
+
+            <input
+              type='text'
+              placeholder='Поиск по имени или e-mail'
+              onChange={e => {
+                this.setState({
+                  searchQuery: e.target.value
+                })
+              }}
+              onKeyPress={async e => {
+                if (e.key === 'Enter') {
+                  const users = await this.fetchUsers(this.state.searchQuery)
+                  this.setState({
+                    users
+                  })
+                }
+              }} />
+
+            {this.state.users.map(user => (
+              <div className='rating-list__item' key={user._id}>
+                <UserInline user={user} />
+              </div>
+            ))}
+          </div>
+        </Panel>
+      </FeedLayout>
+    )
+  }
+}
+
+export default Page(NPSRatings, {
+  title: 'Рейтинг NPS'
+})
